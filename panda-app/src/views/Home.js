085b453ac3b00a04fac3436262aabbfe1c5e7dd9
BD <bb.pambhar@gmail.com>
2020-05-25 20:17:07 +0530

    updated

--- panda-app/src/views/Home.js
+++ panda-app/src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNews } from '../redux/actions/news';
@@ -14,17 +14,32 @@ const Home = () => {
         news: state.postsReducer.news
     }));
 
-    console.log(params.countries);
+
 
     useEffect(() => {
         dispatch(fetchNews(params.country));
     }, [dispatch, params.country]);
+    console.log(news);
+
+    const [singlenews, Setsinglenews] = useState(null);
+
+    var marvelHeroes = news.filter(function (hero) {
+        return hero.author == "Clarín.com";
+    });
 
 
-
-
-    const countries = ["Argentina", "Australia", "Belgium", "Brazil", "Canada", "Colombia", "China", "Egypt", "Hong Kong", "India", "Indonesia", "Italy", "Japan", "Malaysia", "New Zealand", "Norway", "Poland", "Romania", "Singapore"];
-    // console.log(countries);
+    const countries = [
+        { key: "ar", value: "Argentina" },
+        { key: "au", value: "Australia" },
+        { key: "be", value: "Belgium" },
+        { key: "br", value: "Brazil" },
+        { key: "ca", value: "Canada" },
+        { key: "co", value: "Colombia" },
+        { key: "cn", value: "China" },
+        { key: "eg", value: "Egypt" },
+        { key: "hk", value: "Hong Kong" },
+        { key: "in", value: "India" },
+        { key: "id", value: "Indonesia" }];
 
     return (
         <div className="container-md border">
@@ -33,7 +48,7 @@ const Home = () => {
                 <div className="col-sm-2 border"><div>
                     {countries !== null &&
                         countries.map((country) => (
-                            <div onClick={() => dispatch(fetchNews(country))}>{country}</div>
+                            <div onClick={() => dispatch(fetchNews(country.key))}>{country.value}</div>
                         ))}
                 </div></div>
                 <div className="col-sm-4 border">
@@ -44,8 +59,9 @@ const Home = () => {
                                 <div>
                                     {news !== null &&
                                         news.map(news => (
-                                            <div key={news.id}>
+                                            <div key={news.id} onClick={() => dispatch(fetchNews())}>
                                                 <div>{news.source.name}</div>
+                                                <div>{news.title}</div>
                                             </div>
                                         ))}
                                 </div>
@@ -53,7 +69,16 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="col-sm-6 border">
+                    <div>
+                        {loading ? (
+                            <div>Loading...</div>
+                        ) : (
+                                <div>
+                                    <div>{singlenews.title}</div>
 
+                                </div>
+                            )}
+                    </div>
                 </div>
             </div>
         </div>
