f3cc0f9339293da8ee44da9597e89a4b3917bedb
BD <bb.pambhar@gmail.com>
2020-05-25 18:36:46 +0530

    app update 25/05/2020

--- panda-app/src/views/Home.js
+++ panda-app/src/views/Home.js
@@ -1,27 +1,31 @@
 import React, { useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNews } from '../redux/actions/news';
-import { Button } from "reactstrap";
+import { useParams } from '@reach/router';
 
 const Home = () => {
+
     const dispatch = useDispatch();
+    const params = useParams();
+
     const { loading, news } = useSelector(state => ({
         loading: state.postsReducer.loading,
         news: state.postsReducer.news
     }));
 
+    console.log(params.countries);
+
     useEffect(() => {
-        dispatch(fetchNews());
-    }, [dispatch]);
-
-    console.log(loading, news);
+        dispatch(fetchNews(params.country));
+    }, [dispatch, params.country]);
 
 
 
-    const countries = ["Bussiness", "Entertainment", "Health", "Science", "Sport", "Technology"];
-    console.log(countries);
+
+    const countries = ["Argentina", "Australia", "Belgium", "Brazil", "Canada", "Colombia", "China", "Egypt", "Hong Kong", "India", "Indonesia", "Italy", "Japan", "Malaysia", "New Zealand", "Norway", "Poland", "Romania", "Singapore"];
+    // console.log(countries);
+
     return (
         <div className="container-md border">
             <div className="row border">Menu</div>
@@ -29,7 +33,7 @@ const Home = () => {
                 <div className="col-sm-2 border"><div>
                     {countries !== null &&
                         countries.map((country) => (
-                            <div>{country}</div>
+                            <div onClick={() => dispatch(fetchNews(country))}>{country}</div>
                         ))}
                 </div></div>
                 <div className="col-sm-4 border">
@@ -41,22 +45,16 @@ const Home = () => {
                                     {news !== null &&
                                         news.map(news => (
                                             <div key={news.id}>
-                                                <div>{news.author}</div>
-                                                <div>{news.content}</div>
-                                                <div>{news.description}</div>
-                                                <div>{news.publishedAt}</div>
-                                                <div>{news.source.id}</div>
                                                 <div>{news.source.name}</div>
-                                                <div>{news.title}</div>
-                                                <div>{news.url}</div>
-                                                <div>{news.urlToImage}</div>
                                             </div>
                                         ))}
                                 </div>
                             )}
                     </div>
                 </div>
-                <div className="col-sm-6 border"></div>
+                <div className="col-sm-6 border">
+
+                </div>
             </div>
         </div>
     );
