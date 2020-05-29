b47ff46fa80d18a9e0e62746d295683fcb4d8227
BD <bb.pambhar@gmail.com>
2020-05-29 19:54:09 +0530

    updated (29/05/2020)

--- panda-app/src/Components/Rightbar.js
+++ panda-app/src/Components/Rightbar.js
@@ -4,14 +4,19 @@ import moment from 'moment';
 const Rightbar = ({ singlenews }) => {
     const { title, description, urlToImage, publishedAt, url } = singlenews;
 
-    const date = moment(publishedAt).fromNow()
-    // console.log(date);
+    const date1 = moment(publishedAt).fromNow();
+    console.log(date1);
+
+    // const date = new Date();
+    // console.log(date)
+    // const date2 = moment(date).format("DD-MM-yyyy HH:mm:ss")
+    // console.log(date2);
 
 
     return (
         <div className={title === undefined ? "display-none scroll-panel" : ""}>
             <div><h4>{title}</h4></div>
-            <div>{date}</div>
+            <div>{date1}</div>
             <div><img src={urlToImage} className="newsimg"></img></div>
             <div>{description}</div>
             <div><a href={url}>{title === undefined ? "" : "Read More"}</a></div>
